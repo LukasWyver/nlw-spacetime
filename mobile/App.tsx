f2fe71cde3c01b0f03a87f323fd4b3159d53d837
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-05-21 13:28:30 -0300

    mobile config

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className=" flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-gray-50">
+        web<Text className="font-black text-sky-500">2</Text>midia
+      </Text>
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
