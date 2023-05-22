3b6f3c0903f9ec5a4545eeda9a224803775e5db1
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-05-21 21:45:06 -0300

    install expo-fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className=" flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-gray-50">
-        web<Text className="font-black text-sky-500">2</Text>midia
+    <View className=" flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-body text-5xl text-gray-50">
+        web<Text className="font-title text-sky-500">2</Text>midia
       </Text>
       <StatusBar style="light" translucent />
     </View>
