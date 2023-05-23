4450820f9004ac5a1b1907fd652e19f71ecf11b4
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-05-23 00:33:54 -0300

    access_token config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
 app.register(cors, {
-  origin: ['http://localhost:3000'], // true: todas as URLs de front-end poderão acessar nosso back-end
+  origin: true, // http://localhost:3000 // true: todas as URLs de front-end poderão acessar nosso back-end
 })
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
